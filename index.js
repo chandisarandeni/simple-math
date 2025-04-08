7a6e89c77852c32325cd616734c3f1fe9f447cfd
chandisarandeni <chandisarandeni2003@gmail.com>
2025-04-08 14:59:45 +0530

    Implement animation refactor & help
    Implement animation sequence and add help instructions

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ import { subAnimationDevelopedBy } from "./Animations/base-animation.js";
 import { basicLine } from "./Animations/base-animation.js";
 import { showLoadingAnimation } from "./Animations/loading-animation.js";
 import showProgressBar from "./Animations/pogress-bar.js";
+import { helpInstructions } from "./instructions/help-instructions.js";
 
+async function runAnimations() {
+  // Loading animation loading process
+  // Baisc word "SIMPLE MATH" animation
+  baseAnimation();
+  await showLoadingAnimation(); // wait for sec
 
-baseAnimation();
-await showLoadingAnimation();
-subAnimationDevelopedBy();
-await showLoadingAnimation();
-basicLine();
-console.log("");
-showProgressBar();
+  // Developed by animation
+  subAnimationDevelopedBy();
+  await showLoadingAnimation(); // wait for sec
+  
+  // Basic line animation
+  basicLine();
+  console.log("");
+  showProgressBar();
+
+  // Wait for 3 seconds before showing the help instructions
+  setTimeout(() => {
+    console.log("");
+    helpInstructions();
+  }, 3800); // Adjust the timeout as needed
+}
+
+runAnimations();
